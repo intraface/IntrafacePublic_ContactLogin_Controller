9496ff67919e67cfe9fab299cb74d7b7dba4e24a
Lars Olesen <lars@intraface.dk>
2010-05-23 10:22:11 +0000

    Hardcoded link to gravatar image.

--- src/IntrafacePublic/ContactLogin/templates/navigation.tpl.php
+++ src/IntrafacePublic/ContactLogin/templates/navigation.tpl.php
@@ -1,5 +1,5 @@
 <form style="float: right; width: 80px; align: center;" method="post" action="<?php e(url('../logout')); ?>">
-<a href="http://en.gravatar.com/site/signup" title="<?php e(t('Get your own gravatar')); ?>"><img style="border: none; align: center;" src="http://www.gravatar.com/avatar/<?php e(md5($email)); ?>?s=80&amp;d=wavatar" height="80" width="80" /></a><p><input type='submit' value='Log out' /></p></form>
+<a href="http://en.gravatar.com/site/signup" title="<?php e(t('Get your own gravatar')); ?>"><img style="border: none; align: center;" src="http://www.gravatar.com/avatar/<?php e(md5($email)); ?>?s=80&amp;d=http://intraface.dk/core/images/icons/gravatar.png" height="80" width="80" /></a><p><input type='submit' value='Log out' /></p></form>
 
 <h1><?php e(t('Contact login')); ?></h1>
 
